c29f0d6ab947f1c5cb119b0edfe6bf1ad3e8f5f5
LessSiming <2943089928@qq.com>
2018-01-27 14:53:27 +0800

    提交项目

---
+++ NotesStudy/AppDelegate/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NotesStudy
+//
+//  Created by Lj on 2018/1/27.
+//  Copyright © 2018年 lj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NotesStudy/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NotesStudy
+//
+//  Created by Lj on 2018/1/27.
+//  Copyright © 2018年 lj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
